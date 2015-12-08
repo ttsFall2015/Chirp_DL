e7be194d222bf39e5a157e9a5d0f837bc307c640
Shane Barringer <shane.barringer@outlook.com>
2015-12-08 11:06:02 -0500

    adds routes for devise

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ class TweetsController < ApplicationController
 
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!
 
   def index
     @tweets = Tweet.all
@@ -20,10 +21,9 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
 
-    responds_to do |format|
+    respond_to do |format|
       if @tweet.save
-        format.html {redirect_to @tweet,
-        notice: 'Tweet was successfully created.'}
+        format.html {redirect_to @tweet, notice: 'Tweet was successfully created.'}
       else
         format.html {render :new}
       end
@@ -32,13 +32,13 @@ class TweetsController < ApplicationController
 
 private
 
-      def set_tweet
-        @tweet = Tweet.find(params[:id])
-      end
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
 
-   def tweet_params
-     params.require(:tweet).permit(:message, :user_id)
-   end
-
+ def tweet_params
+   params.require(:tweet).permit(:message, :user_id)
  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ resources :tweets
 
 root "tweets#index"
 
+devise_for :users
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
